faa47927959a77e98a85ab94783b0f8f8e0120cd
Nikita <nstegura@inbox.ru>
2020-11-06 22:01:08 +0300

    Оформление+условие

--- main.py
+++ main.py
@@ -10,9 +10,17 @@ def get_weather(place):
     observation = mgr.weather_at_place(place)
     w = observation.weather
 
-    temp = w.temperature('celsius')['temp']
+    temp1 = w.temperature('celsius')['temp']
+    print(temp1)
     #print("В городе " + place + " сейчас " + str(temp) + " градусов!")
-    return "В городе " + place + " сейчас " + str(temp) + " градусов!"
+    return "В городе " + place + " сейчас " + str(temp1) + " градусов! \f"  +  get_answer(temp1)
+
+def get_answer(temp1):
+    if temp1 > 10:
+        return "T-SHIRT!!!!"
+    else:
+        return "COAT!!!!"
+
 
 eel.init("web")
 
